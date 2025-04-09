2852228d74451619a05fe08cd74070973cdfe5c9
MUKESH <mukeshsinghjsr1995@gmail.com>
2025-04-09 22:58:38 +0530

    changed name

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "🚀 Hello from your Dockerized Flask app!"
+    return "🚀 Hello Mukesh Singh from your Dockerized Flask app!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
