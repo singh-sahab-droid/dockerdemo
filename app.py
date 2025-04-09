6e7c676cd17b1fcc4c626be8a137566cc627376d
MUKESH <mukeshsinghjsr1995@gmail.com>
2025-04-09 23:02:52 +0530

    changed from bash

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "🚀 Hello Mukesh Singh from git hub gui changed file for  your Dockerized Flask app!"
+    return "🚀 Hello Mukesh Singh from git hub gui changed file for  your Dockerized Flask app! and now u all gonna see the change in me though bash"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
